b5117e6f1b8648ca8c3939444e1edb9dc310eeaf
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-03-07 13:52:39 -0600

    more logs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,12 +8,9 @@ export default function Home() {
 
       <h1>Lifecycle w Suspense Debugging</h1>
 
+      {/* comment DataComp-1 out, reload, and see terminal logs for issue. onCleanup only called for DataCompWithInnerSuspense-2 */}
       <Suspense>
         <DataComp name="DataComp-1" wait={1000} />
-      </Suspense>
-
-      <Suspense>
-        <DataComp name="DataComp-2" wait={500} />
       </Suspense>
 
       <DataCompWithInnerSuspense
@@ -32,6 +29,8 @@ export default function Home() {
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const resource = (name: string, wait: number) => {
+  console.log(`call:${name}.resource`);
+
   onMount(() => {
     console.log(`onMount:${name}.resource`);
   });
@@ -50,6 +49,8 @@ const resource = (name: string, wait: number) => {
 };
 
 const DataComp = (props: { name: string; wait: number }) => {
+  console.log(`call:${props.name}`);
+
   const [data] = resource(props.name, props.wait);
 
   onMount(() => {
@@ -64,6 +65,8 @@ const DataComp = (props: { name: string; wait: number }) => {
 };
 
 const DataCompWithInnerSuspense = (props: { name: string; wait: number }) => {
+  console.log(`call:${props.name}`);
+
   const [data] = resource(props.name, props.wait);
 
   onMount(() => {
